8bc4cf7cb808f883217cc0e1e3696370aa47d5ad
Abhishek SAINI <saini.abhishek@hotmail.com>
2022-10-12 22:41:17 +0200

    rest based api

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './app/App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./app/App";
+import reportWebVitals from "./reportWebVitals";
 import reducers from "./reducers";
-import {legacy_createStore as createStore} from "redux";
-import {Provider} from "react-redux";
+import {
+  legacy_createStore as createStore,
+  applyMiddleware,
+  compose,
+} from "redux";
+import { Provider } from "react-redux";
+import reduxThunk from "redux-thunk";
 
-const store = createStore(reducers);
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(reducers, compose(applyMiddleware(reduxThunk)));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-        <App />
+      <App />
     </Provider>
   </React.StrictMode>
 );
