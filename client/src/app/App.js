f984f420e5740054a10b3f88fdfd417fee7e0c45
Abhishek SAINI <saini.abhishek@hotmail.com>
2022-10-16 14:01:05 +0200

    histroy, stream list component and router

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,17 +1,18 @@
-import './App.css';
-import { BrowserRouter, Route } from 'react-router-dom';
-import StreamCreate from '../components/streams/StreamCreate';
-import StreamEdit from '../components/streams/StreamEdit';
-import StreamDelete from '../components/streams/StreamDelete';
-import StreamList from '../components/streams/StreamList';
-import StreamShow from '../components/streams/StreamShow';
+import "./App.css";
+import { Router, Route } from "react-router-dom";
+import StreamCreate from "../components/streams/StreamCreate";
+import StreamEdit from "../components/streams/StreamEdit";
+import StreamDelete from "../components/streams/StreamDelete";
+import StreamList from "../components/streams/StreamList";
+import StreamShow from "../components/streams/StreamShow";
 import Header from "../components/Header";
+import history from "../history";
 
 function App() {
   return (
     <div className="App">
       <div className="ui container">
-        <BrowserRouter>
+        <Router history={history}>
           <div>
             <Header />
             <Route path="/" exact component={StreamList} />
@@ -20,7 +21,7 @@ function App() {
             <Route path="/streams/delete" exact component={StreamDelete} />
             <Route path="/streams/show" exact component={StreamShow} />
           </div>
-        </BrowserRouter>
+        </Router>
       </div>
     </div>
   );
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,10 +1,11 @@
-import { SIGN_IN, SIGN_OUT } from '../actions/types';
+import { SIGN_IN, SIGN_OUT } from "../actions/types";
 
 const INITIAL_STATE = {
   isSignedIn: null,
-  userId: null
+  userId: null,
 };
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case SIGN_IN:
